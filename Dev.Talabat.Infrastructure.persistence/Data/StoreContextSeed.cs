a7a5b94dda24ee1f8aa107c951f0ddd6a735f0f8
OmarNabil <omarnabilsaber58@gmail.com>
2025-12-07 17:37:52 +0200

    adding seeding for categories and brands

--- Dev.Talabat.Infrastructure.persistence/Data/StoreContextSeed.cs
+++ Dev.Talabat.Infrastructure.persistence/Data/StoreContextSeed.cs
@@ -9,7 +9,8 @@ namespace Dev.Talabat.Infrastructure.persistence.Data
     public static class StoreContextSeed
     {
         public static async Task SeedAsync (StoreContext dbContext, ILoggerFactory loggerFactory)
-        { 
+        {
+            #region seeding Brands data 
             if (!dbContext.ProductBrands.Any())
             {
                 try
@@ -27,10 +28,57 @@ namespace Dev.Talabat.Infrastructure.persistence.Data
                 catch (Exception ex)
                 {
                     var logger = loggerFactory.CreateLogger("StoreContextSeed");
-                    logger.LogError("Error seeding ProductBrands: ");
+                    logger.LogError("Error seeding Product Brands: ");
                     logger.LogError(ex.Message);
                 }
             }
+            #endregion
+
+            #region seeding categories data
+            if (!dbContext.ProductCategories.Any())
+            {
+                try
+                {
+                    var categoriesFilePath = "../Dev.Talabat.Infrastructure.persistence/Data/Seeds/categories.json";
+                    var categoriesData = await File.ReadAllTextAsync(categoriesFilePath);
+                    var categories = JsonSerializer.Deserialize<List<ProductCategory>>(categoriesData);
+                    if (categories is not null && categories.Any())
+                    {
+                        await dbContext.ProductCategories.AddRangeAsync(categories);
+                        await dbContext.SaveChangesAsync();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger("StoreContextSeed");
+                    logger.LogError("Error seeding Product Categories: ");
+                    logger.LogError(ex.Message);
+                }
+            }
+            #endregion
+
+            #region seeding products data
+            if (!dbContext.Products.Any())
+            {
+                try
+                {
+                    var productsFilePath = "../Dev.Talabat.Infrastructure.persistence/Data/Seeds/products.json";
+                    var productsData = await File.ReadAllTextAsync(productsFilePath);
+                    var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+                    if (products is not null && products.Any())
+                    {
+                        await dbContext.Products.AddRangeAsync(products);
+                        await dbContext.SaveChangesAsync();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger("StoreContextSeed");
+                    logger.LogError("Error seeding Products: ");
+                    logger.LogError(ex.Message);
+                }
+            } 
+            #endregion
         }
     }
 }
